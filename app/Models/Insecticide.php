5b1eab49fd4a426dc6ce511e1243d78346431e19
lencet-344 <odoricozeleherrera@gmail.com>
2025-09-13 13:48:09 -0600

    feat: agregar modelos para Agronomic_expense, Area, Crop, Crop_type, Customer, Employee, Farm, Harvest e Insecticide

---
+++ app/Models/Insecticide.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Insecticide extends Model
+{
+    use HasFactory;
+}
