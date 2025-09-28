1e00ebbe673147c51e3110834df760a087941503
lencet-344 <odoricozeleherrera@gmail.com>
2025-09-28 13:42:11 -0600

    fix:

--- app/Http/Requests/Post_harvestRequest.php
+++ app/Http/Requests/Post_harvestRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class PostHarvest extends FormRequest
+class Post_harvestRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
