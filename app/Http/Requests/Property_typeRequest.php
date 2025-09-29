e1b57b2abad7978dabbec2054bbebece6f77b78c
lencet-344 <odoricozeleherrera@gmail.com>
2025-09-28 19:37:38 -0600

    fix:

--- app/Http/Requests/Property_typeRequest.php
+++ app/Http/Requests/Property_typeRequest.php
@@ -8,18 +8,26 @@ use Illuminate\Validation\Rule;
 
 class Property_typeRequest extends FormRequest
 {
+
+    public function authorize(): bool
+    {
+        return true;
+    }
     /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
      */
+
+    
+    
     public function rules(): array
     {
         return [
             'name' => ['required', 'string', 'max:255'],
             'description' => ['required', 'string'],
             
-         ];
+        ];
     }
 
     /**
